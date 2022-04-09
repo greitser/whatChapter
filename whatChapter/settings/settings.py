6df8b4b1783c6355cb59338093e9d078e76b4174
greitser <77767942+greitser@users.noreply.github.com>
2022-04-10 00:25:24 +0700

    Update settings.py

--- whatChapter/settings/settings.py
+++ whatChapter/settings/settings.py
@@ -29,7 +29,7 @@ POSTGRES_PORT = env.str("POSTGRES_PORT")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("DJANGO_SEKRET_KEY")
+SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
